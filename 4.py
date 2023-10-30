19d87cb0a7d7a7d2b4a3e7cc7dbe33036a30782f
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-10-30 18:43:45 +0530

    first commit

---
+++ 4.py
@@ -0,0 +1,8 @@
+roman={1:'i',2:'ii'}
+d,r=roman
+print(d,r)
+
+#1 2
+#In summary, the code unpacks the keys of the `roman` dictionary
+# into variables `d` and `r`, resulting in `d` containing `1` and `r` containing `2`, and then it prints these values. 
+# The values associated with the keys in the dictionary (i.e., 'i' and 'ii') are ignored during the unpacking. 
