01bd6b75aaa0f719d7b2f90dc40af19faa54082e
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-10-30 18:48:27 +0530

    first commit

---
+++ 5.py
@@ -0,0 +1,7 @@
+my_list = [10, 3, 12, 30, 77]
+
+# Using slice() function to create a slice
+my_slice = my_list[1:3]  # This will slice elements at index 1 and 2 (3rd element not included)
+
+# Printing the slice
+print(my_slice)
