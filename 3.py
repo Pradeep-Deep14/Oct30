698a87151e082b7b9859baa71c039fd1de7ce219
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-10-30 18:37:36 +0530

    first commit

---
+++ 3.py
@@ -0,0 +1,9 @@
+x=100
+y=50
+print(x and y)
+
+
+#50
+#So, the output of this code is `50`, 
+# which is the value of `y` because the `and` operator 
+# returned the second value when the first value (`x`) was truthy.
